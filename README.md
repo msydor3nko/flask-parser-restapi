2f5bf5761ac135a5c6f1681704bef64ba7837630
Mykhailo Sydorenko <msydor3nko@gmail.com>
2021-01-14 21:32:57 +0200

    docs added parser refactored

---
+++ README.md
@@ -0,0 +1,69 @@
+# Flask Parser and RestAPI
+The simple mini app based on Python Flask.
+
+### Key features:
+- parsing data with products and their reviews from CSV files (datasets attached)
+- saving parsed data into PostgreSQL with one-to-many relationships
+- getting DB access to product info and reviews with pagination by 'id' using API GET-endpoint (the response cached on 10 sec.)
+- creating new product review into DB by product 'id' using API PUT-endpoint
+
+
+## What do you need
+
+**You need:** Git, Python3, pip, PostgreSQL (or just use SQLite).
+
+
+## Guides
+
+### Setup environment
+
+* Clone the App repository using Git
+
+`git clone https://github.com/msydor3nko/flask-parser-restapi.git`
+
+* Enter to the 'flask-parser-restapi' directory
+
+`cd flask-parser-restapi`
+
+* Create and activate virtual environment
+
+`python3 -m venv venv`
+
+`source venv/bin/activate`
+
+* Install all required libraries from 'requirements.txt'
+
+`pip install -r requirements.txt`
+
+### Datasets parsing 
+
+* run 'parser.py' to parse datasets and save results into database:
+
+`python parser.py`
+
+
+### API using
+
+* Run Flask app to use API:
+
+`flask run`
+
+* To make requests use endpoints with standard Flask options
+
+GET: `http://127.0.0.1:5000/api/products/<id>`
+
+PUT: `http://127.0.0.1:5000/api/reviews/<id>`
+
+
+# Note
+You need setup PostgreSQL connection in '.env' file to work with data.
+
+You can choose SQLite (using by default) as alternative DB using command:
+
+`flask db init`
+
+Migrations are also available if it needed:
+
+`flask db migrate -m 'init db'`
+
+`flask db upgrade`
--- parser.py
+++ parser.py
@@ -4,46 +4,40 @@ import csv
 from api.models import Product, Review
 
 
-products_csv = os.path.join(os.path.abspath("datasets"), "products.csv")
-reviews_csv = os.path.join(os.path.abspath("datasets"), "reviews.csv")
-
-
 class Parser(object):
-
-    def __init__(self, csv_name: str):
-        self.csv_file = csv_name
+    def __init__(self, csv_file_name: str, is_products_data: bool = True):
+        self.csv_file = csv_file_name
+        self.is_products = is_products_data
 
     def run(self):
         data_items = self.csv_rows_to_dicts(self.csv_file)
         for item in data_items:
-            self.save_item_to_db(item)
+            if self.is_products:
+                self.save_products_into_db(item)
+            else:
+                self.save_reviews_into_db(item)
 
     def csv_rows_to_dicts(self, csv_file: str):
         with open(csv_file) as data:
-            for data_item in csv.DictReader(data):
-                yield data_item
+            for row_data in csv.DictReader(data):
+                yield row_data
 
-    def save_item_to_db(self, data_item: dict):
-        print(data_item)
+    def save_products_into_db(self, item: dict):
+        product = Product()
+        product.save_parsed_product_to_db(item)
 
-
-def parse_and_save_products(filename: str):
-    with open(filename) as data:
-        for item_data in csv.DictReader(data):
-            product = Product()
-            product.save_parsed_product_to_db(item_data)
-
-
-def parse_and_save_reviews(filename: str):
-    with open(filename) as data:
-        for item_data in csv.DictReader(data):
-            review = Review()
-            review.save_parsed_review_to_db(item_data)
+    def save_reviews_into_db(self, item: dict):
+        review = Review()
+        review.save_parsed_review_to_db(item)
 
 
 if __name__ == "__main__":
-    # parse_and_save_products(products_csv)
-    parse_and_save_reviews(reviews_csv)
+    products_csv = os.path.join(os.path.abspath("datasets"), "products.csv")
+    reviews_csv = os.path.join(os.path.abspath("datasets"), "reviews.csv")
 
-    # parser = Parser(products_csv)
-    # parser.run()
+    products_parser = Parser(csv_file_name=products_csv, is_products_data=True)
+    reviews_parser = Parser(csv_file_name=reviews_csv, is_products_data=False)
+
+    products_parser.run()
+    reviews_parser.run()
+
